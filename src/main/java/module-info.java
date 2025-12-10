006cf54e6bf2196787e79c9a06261480b148e330
Simon Toivola <149212781+bamsen54@users.noreply.github.com>
2025-12-10 16:24:00 +0100

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,16 +1,12 @@
 module com.simonsgearhubjavafx {
-    // Dessa krävs för JavaFX:s kärnfunktionalitet
+    
     requires javafx.controls;
     requires javafx.fxml;
 
     requires javafx.graphics;
     requires javafx.base;
 
-    // Denna rad har tagits bort: requires com.simonsgearhubjavafx;
-
-    // Öppnar modulen för FXML-laddaren
     opens com.simonsgearhubjavafx to javafx.fxml;
-
-    // Exporterar paketet så att andra moduler kan använda det
+    
     exports com.simonsgearhubjavafx;
 }
